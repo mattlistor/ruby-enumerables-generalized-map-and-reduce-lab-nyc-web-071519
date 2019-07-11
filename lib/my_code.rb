76abc7214bdc3c9574c059ef6a370ad64b6230cb
Matthew Listor <mattlistor@gmail.com>
2019-07-11 19:54:56 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
-# Your Code Here
+def map(source_array) 
+  source_array.length.times do |i|
+    source_array[i] = yield(source_array[i])
+  end
+  return source_array
+end
+
+def reduce(source_array, starting_point = 0)
+  source_array.length.times do |i|
+    if yield(source_array[i], starting_point)
+      starting_point = yield(source_array[i], starting_point)
+    else
+      return false
+    end
+  end
+  return starting_point
+end
+
+
